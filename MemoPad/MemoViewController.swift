7c5c8da21b9fe24a91a60cf97e9fafc746d7e03a
Shintaro Tsuge <shin.abe_1027@icloud.com>
2024-07-20 17:46:12 +0900

    Initial Commit

---
+++ MemoPad/MemoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MemoViewController.swift
+//  MemoPad
+//
+//  Created by 柘植紳太郎 on 2024/07/19.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    var saveDeta: UserDefaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleTextField.text = saveDeta.object(forKey: "title") as? String
+        contentTextView.text = saveDeta.object(forKey: "content") as? String
+        titleTextField.delegate = self
+        
+        func textFiledShoulReturn(_ textFiled: UITextField) -> Bool{
+            textFiled.resignFirstResponder()
+            return true
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveMemo(_ sender: Any) {
+        saveDeta.set(titleTextField, forKey: "title")
+        saveDeta.set(contentTextView, forKey: "content")
+        
+        let alert: UIAlertController = UIAlertController(title: "保存", message: "メモの保存が完了しました。", preferredStyle: .alert )
+        
+        alert.addAction(
+            UIAlertAction(title: "OK",
+                          style: .default,
+                          handler: { action in
+                              self.navigationController?.popViewController(animated: true)
+                              
+                              print("OKが押されました。")
+                          })
+        )
+        present(alert,animated: true,completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MemoPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoPad
+//
+//  Created by 柘植紳太郎 on 2024/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
